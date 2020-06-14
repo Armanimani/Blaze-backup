d1c4746bb5e3291961d7b9d05b4e03ab2422497d
Arman Imani <armanimani87@gmail.com>
2020-06-14 12:51:29 +0200

    Added the basic types and test for the premitive types. Removed the int 128 as it might not be available in all platforms

--- Blaze/Engine/Base/include/Blaze/Engine/Base/types.hpp
+++ Blaze/Engine/Base/include/Blaze/Engine/Base/types.hpp
@@ -6,13 +6,11 @@ namespace blaze::base
 	using Int16 = __int16;
 	using Int32 = __int32;
 	using Int64 = __int64;
-	using Int128 = __int128;
 
 	using UInt8 = unsigned __int8;
 	using UInt16 = unsigned __int16;
 	using UInt32 = unsigned __int32;
 	using UInt64 = unsigned __int64;
-	using UInt128 = unsigned __int128;
 
 	using Float = float;
 	using Double = double;
