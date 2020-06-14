29d229710aceb3e8a0e6bde2fcc17bd7e3821f42
Arman Imani <armanimani87@gmail.com>
2020-06-14 13:05:32 +0200

    The types are now using the types from the cstdlib header instead of relying on compiler typoes

--- Blaze/Engine/Base/include/Blaze/Engine/Base/types.hpp
+++ Blaze/Engine/Base/include/Blaze/Engine/Base/types.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
+#include <cstdlib>
+
 namespace blaze::base
 {
-	using Int8 = __int8;
-	using Int16 = __int16;
-	using Int32 = __int32;
-	using Int64 = __int64;
+	using Int8 = _int8;
+	using Int16 = _int16;
+	using Int32 = _int32;
+	using Int64 = _int64;
 
-	using UInt8 = unsigned __int8;
-	using UInt16 = unsigned __int16;
-	using UInt32 = unsigned __int32;
-	using UInt64 = unsigned __int64;
+	using UInt8 = unsigned _int8;
+	using UInt16 = unsigned _int16;
+	using UInt32 = unsigned _int32;
+	using UInt64 = unsigned _int64;
 
 	using Float = float;
 	using Double = double;
