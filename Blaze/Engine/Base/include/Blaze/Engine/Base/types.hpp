374c0d4a8b82131de564cbd0e5fa383a87ca3e53
Arman Imani <armanimani87@gmail.com>
2020-06-14 12:19:26 +0200

    Added the basic types for int and unsigned int

---
+++ Blaze/Engine/Base/include/Blaze/Engine/Base/types.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace blaze::base
+{
+	using Int8 = __int8;
+	using Int16 = __int16;
+	using Int32 = __int32;
+	using Int64 = __int64;
+	using Int128 = __int128;
+
+	using UInt8 = unsigned __int8;
+	using UInt16 = unsigned __int16;
+	using UInt32 = unsigned __int32;
+	using UInt64 = unsigned __int64;
+	using UInt128 = unsigned __int128;
+}
