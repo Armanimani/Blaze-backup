c471658ee3a11567c41283f597db8fb8ff4c66b4
Arman Imani <armanimani87@gmail.com>
2020-06-24 19:25:17 +0200

    Defined the idea of a descriptive system that defines the components that will be accessed by this system

--- Blaze/Engine/Core/include/Blaze/Engine/Core/Application/ISystem.hpp
+++ Blaze/Engine/Core/include/Blaze/Engine/Core/Application/ISystem.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Blaze/Engine/Core/Types/base_types.hpp"
+#include "Blaze/Engine/Core/Types/TypeIdentifier.hpp"
+#include <array>
 
 namespace blaze
 {
@@ -12,5 +14,32 @@ namespace blaze
 		virtual void initialize() noexcept = 0;
 		virtual void update(Float delta_time) noexcept = 0;
 		virtual void finalize() noexcept = 0;
+	};
+
+	template<typename...Component>
+	struct ComponentPack
+	{
+		static std::array<UInt32, sizeof...(Component)> identification()
+		{
+			std::array<UInt32, sizeof...(Component)> result{};
+			const std::initializer_list<UInt32> list = { TypeIdentifier::get<Component>()... };
+
+			auto counter = 0;
+			for (auto i = list.begin(); i != list.end(); ++i)
+			{
+				result[counter] = *i;
+				counter++;
+			}
+
+			return result;
+		}
+	};
+
+	template<typename MutableComponents, typename ImmutableComponents>
+	class DescriptiveSystem: ISystem
+	{
+	public:
+		using mutable_components = MutableComponents;
+		using immutable_components = ImmutableComponents;
 	};
 }
