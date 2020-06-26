36ec6867c2af8f2353dcd13acb66c37e0cd87db0
Arman Imani <armanimani87@gmail.com>
2020-06-27 00:09:48 +0200

    removed the default initialization of device

--- Blaze/Engine/PlatformDirectX12/private/DX12DeviceFactory.cpp
+++ Blaze/Engine/PlatformDirectX12/private/DX12DeviceFactory.cpp
@@ -6,7 +6,7 @@ namespace blaze
 {
 	Microsoft::WRL::ComPtr<ID3D12Device6> DX12DeviceFactory::create() noexcept
 	{
-		Microsoft::WRL::ComPtr<ID3D12Device6> device{};
+		Microsoft::WRL::ComPtr<ID3D12Device6> device;
 
 		if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&device))))
 			ConsoleLogger::logCritical(k_dx12_channel, "Unable to create DirectX12 device!");
