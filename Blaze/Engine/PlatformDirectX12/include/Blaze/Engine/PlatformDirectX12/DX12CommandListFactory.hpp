d1341220bf8a793367a522400fc3ac1f9f4a0e96
Arman Imani <armanimani87@gmail.com>
2020-06-26 21:11:11 +0200

    Added a factory class to create a command list

---
+++ Blaze/Engine/PlatformDirectX12/include/Blaze/Engine/PlatformDirectX12/DX12CommandListFactory.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Blaze/Engine/PlatformDirectX12/common.hpp"
+
+namespace blaze
+{
+	class DX12CommandListFactory
+	{
+	public:
+		static Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList5> create(
+			const Microsoft::WRL::ComPtr<ID3D12Device6>& device,
+			const Microsoft::WRL::ComPtr<ID3D12CommandAllocator>& allocator,
+			D3D12_COMMAND_LIST_TYPE type) noexcept;
+	};
+}
